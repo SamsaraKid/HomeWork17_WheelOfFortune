d4006f5ebdd9784270a686fc284c92914e93c306
koany <koroliev@yandex.ru>
2023-04-25 00:10:06 +0300

    Просто коммит, ничего не готово 3

--- js/main.js
+++ js/main.js
@@ -12,20 +12,76 @@ const words = [
 ]
 
 let word = []
+let questNum = 0
 let secret = []
+let scores = 0
+let moveScores = 0
 
 $(document).ready(start)
 
+// подготовка игры
 function start() {
-    let x = Math.floor(Math.random()*words.length)
-    word = words[x].answer.split('')
+    questNum = Math.floor(Math.random()*words.length)
+    word = words[questNum].answer.toUpperCase().split('')
     for (i in word) {
         secret.push('*')
     }
-    $('#question').text(words[x].question)
+    $('#question').text(words[questNum].question)
     $('#word').text(secret.join(' '))
+    nextMove()
 }
 
-function guessLetter() {
-
+// крутим колесо
+function spinWheel() {
+    if ($('#wheelWrap img').classList !== 'spinWheel') {
+        $('#wheelWrap img').addClass('spinWheel')
+    }
+    setTimeout(function (){
+        $('#wheelWrap img').removeClass('spinWheel')
+        moveScores = Math.floor(Math.random()*15+1)*50
+        makeGuess()
+    },3000)
 }
+
+// угадываем букву
+function guess() {
+    let letter = $('#guessLetter').val().toUpperCase()
+    for (w in word) {
+        if (letter == word[w]){
+            secret[w] = letter
+            scores += moveScores
+            $('#scores').text('У вас ' + scores + ' очков')
+        }
+    }
+    $('#word').text(secret.join(' '))
+    nextMove()
+    check()
+}
+
+// проверяем слово
+function check() {
+    if (secret.indexOf('*') == -1) {
+        $('#moveScores').text('Вы победили')
+    }
+}
+
+// подготовка к следующему ходу
+function nextMove() {
+    $('#guessLetter, #submitLetter, #guessWord, #submitWord')
+            .prop('disabled', true)
+    $('#spin').prop('disabled', false)
+    $('#moveScores').text('Крутите барабан')
+    $('#guessLetter').val('')
+    $('#guessWord').val('')
+}
+
+// подготовка к следующему угадыванию
+function makeGuess() {
+    $('#guessLetter, #submitLetter, #guessWord, #submitWord')
+            .prop('disabled', false)
+    $('#spin').prop('disabled', true)
+    $('#moveScores').text('На барабане ' + moveScores + ' очков')
+}
+
+$('#spin').click(spinWheel)
+$('#submitLetter').click(guess)
